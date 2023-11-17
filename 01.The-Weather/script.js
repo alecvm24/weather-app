48d914c0d11fb572344c98f209abb2129d73f2bb
Alec VM <alecvm24@hotmail.com>
2023-11-17 11:42:12 +0100

    Add time and date value

--- 01.The-Weather/script.js
+++ 01.The-Weather/script.js
@@ -5,3 +5,26 @@ const timezone = document.getElementById('time-zone')
 const countryEl = document.getElementById('country')
 const weatherForecastEl = document.getElementById('weather-forecast')
 const currentTempEl = document.getElementById('current-temp')
+
+
+const days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
+const months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
+
+
+setInterval(() => {
+    const time = new Date()
+    const month = time.getMonth()
+    const date = time.getDate()
+    const day = time.getDay()
+    const hour = time.getHours()
+    const hoursIn12HrFormat = hour >= 13 ? hour %12: hour
+    const formattedhours = hoursIn12HrFormat < 10 ? '0' + hoursIn12HrFormat : hoursIn12HrFormat
+    const minutes = time.getMinutes()
+    const formattedMinutes = minutes < 10 ? '0' + minutes : minutes
+    const ampm = hour >=12 ? 'PM' : 'AM'
+
+    timeEl.innerHTML = hoursIn12HrFormat + ':' + minutes + ' ' + '<span id="am-pm">' + ampm + '</span>'
+
+    dateEl.innerHTML = days[day] + ', ' + date + ' ' + months[month]
+
+}, 1000)
